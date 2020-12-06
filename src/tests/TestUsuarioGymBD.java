10c003d302bc515b4a46af5b476294d525db41c2
noraroyo <noraroyo@opendeusto.es>
2020-12-06 19:50:36 +0100

    testUsuarioGymBD acabada

--- src/tests/TestUsuarioGymBD.java
+++ src/tests/TestUsuarioGymBD.java
@@ -2,6 +2,7 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import org.junit.After;
@@ -41,10 +42,49 @@ public class TestUsuarioGymBD {
 		
 	}
 	
+//	@Test
+//	public void test() throws SQLException{
+//		UsuarioGymBD.crearTablaUsuario(gestorBD.getCon());
+//		fail("Not implemented");
+//	}
+	
 	@Test
-	public void test() {
+	public void testInsertarUsuarioGym() {	
 		UsuarioGymBD.crearTablaUsuario(gestorBD.getCon());
-		fail("Not yet implemented");
+		ArrayList<String> clase = new ArrayList<>();
+		clase.add("Zumba");
+		clase.add("Yoga");
+		clase.add("Pilates");
+		
+		UsuarioGymBD.insertarUsuarioGym(gestorBD.getCon(), "111111111A", "Nora", clase);
+		ArrayList<UsuarioGym> r =UsuarioGymBD.todosLosUsuarios(gestorBD.getCon());
+		
+		for(UsuarioGym usuario : r){
+			if(usuario.getDni().equals("111111111A")){
+				assertEquals(usuario.getNombre(), "Nora");
+				
+				ArrayList<String>claser =usuario.getNombreClase();
+				assertEquals(claser.get(0), "Zumba");
+				assertEquals(claser.get(1), "Yoga");
+				assertEquals(claser.get(2), "Pilates");				
+			}
+		}
+		UsuarioGymBD.insertarUsuarioGym(gestorBD.getCon(), "111111111A", null, clase);
+		ArrayList<UsuarioGym> re =UsuarioGymBD.todosLosUsuarios(gestorBD.getCon());
+		
+		
+		for (UsuarioGym usuario : r){
+			if(usuario.getDni().equals("111111111A")){
+				assertNull(usuario.getNombre());
+				
+				ArrayList<String>claser =usuario.getNombreClase();
+				assertNotEquals(claser.get(0), "Zumba");
+				assertNotEquals(claser.get(1), "Yoga");
+				assertNotEquals(claser.get(2), "Pilates");	
+			}
+			
+		}
+
 	}
 	
 	@Test
@@ -86,44 +126,7 @@ public class TestUsuarioGymBD {
 	
 	}
 	
-	@Test
-	public void testInsertarUsuarioGym() {	
-		UsuarioGymBD.crearTablaUsuario(gestorBD.getCon());
-		ArrayList<String> clase = new ArrayList<>();
-		clase.add("Zumba");
-		clase.add("Yoga");
-		clase.add("Pilates");
-		
-		UsuarioGymBD.insertarUsuarioGym(gestorBD.getCon(), "111111111A", "Nora", clase);
-		ArrayList<UsuarioGym> r =UsuarioGymBD.todosLosUsuarios(gestorBD.getCon());
-		
-		for(UsuarioGym usuario : r){
-			if(usuario.getDni().equals("111111111A")){
-				assertEquals(usuario.getNombre(), "Nora");
-				
-				ArrayList<String>claser =usuario.getNombreClase();
-				assertEquals(claser.get(0), "Zumba");
-				assertEquals(claser.get(1), "Yoga");
-				assertEquals(claser.get(2), "Pilates");				
-			}
-		}
-		UsuarioGymBD.insertarUsuarioGym(gestorBD.getCon(), "111111111A", null, clase);
-		ArrayList<UsuarioGym> re =UsuarioGymBD.todosLosUsuarios(gestorBD.getCon());
-		
-		
-		for (UsuarioGym usuario : r){
-			if(usuario.getDni().equals("111111111A")){
-				assertNull(usuario.getNombre());
-				
-				ArrayList<String>claser =usuario.getNombreClase();
-				assertNotEquals(claser.get(0), "Zumba");
-				assertNotEquals(claser.get(1), "Yoga");
-				assertNotEquals(claser.get(2), "Pilates");	
-			}
-			
-		}
-
-	}
+	
 	@Test
 	public void testBorrarUsuarioGym() {	
 		UsuarioGymBD.crearTablaUsuario(gestorBD.getCon());
