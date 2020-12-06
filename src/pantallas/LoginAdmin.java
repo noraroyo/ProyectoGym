8e64ec10fbc692e2603501f92f6a3e231efb5ff3
noraroyo <noraroyo@opendeusto.es>
2020-12-06 20:51:02 +0100

    Login empezado y pantallaPrincipal

--- src/pantallas/LoginAdmin.java
+++ src/pantallas/LoginAdmin.java
@@ -4,40 +4,47 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import logneg.Administrador;
+import logneg.Factura;
+import logneg.UsuarioGym;
+
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JLabel;
 
+/**
+ * 
+ * @author Nora Royo
+ * @author Victor Martinez
+ *
+ */
 public class LoginAdmin extends JFrame {
-
+	private ArrayList<UsuarioGym> usuarioBD = new ArrayList<UsuarioGym>();
+	private ArrayList<Administrador> adminBD = new ArrayList<Administrador>();
+	private ArrayList<Factura> facturasBD = new ArrayList<Factura>();
+	private PantallaPrincipal padre;
 	private JPanel contentPane;
 	private JTextField textField;
 	private JPasswordField passwordField;
+	
+	
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					LoginAdmin frame = new LoginAdmin();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
 	 */
-	public LoginAdmin() {
+	public LoginAdmin(ArrayList<UsuarioGym> usuario, ArrayList<Factura> fac,ArrayList<Administrador> admin, PantallaPrincipal padre) {
+		this.usuarioBD=usuario;
+		this.facturasBD=fac;
+		this.adminBD=admin;
+		this.padre= padre;
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
