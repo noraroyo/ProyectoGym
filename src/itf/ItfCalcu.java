520efed90c2fa211666e37f8e639bf5b31fd24c5
noraroyo <noraroyo@opendeusto.es>
2021-01-13 21:06:20 +0100

    Genéricos: interface Calculator

---
+++ src/itf/ItfCalcu.java
@@ -0,0 +1,11 @@
+package itf;
+
+import java.util.ArrayList;
+
+import logneg.TrabajadorGym;
+
+public interface ItfCalcu <T extends TrabajadorGym>{
+	public abstract ArrayList<Integer> calcu (ArrayList<T>arrayT);
+	
+
+}
