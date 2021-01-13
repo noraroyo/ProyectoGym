142a823ed3ee5aaf36fec75c9f67ab7ff4f1b6e2
noraroyo <noraroyo@opendeusto.es>
2021-01-13 15:57:14 +0100

    Crear un curso nuevo para nuestro fitness centre

---
+++ src/pantallas/NuevoCurso.java
@@ -0,0 +1,28 @@
+package pantallas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class NuevoCurso extends JFrame {
+
+	private JPanel contentPane;
+
+	
+
+	/**
+	 * Create the frame.
+	 */
+	public NuevoCurso(PantallaAdmin padre, int aukera) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
