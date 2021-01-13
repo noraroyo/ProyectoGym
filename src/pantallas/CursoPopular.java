a8eb9aa8ac0135a28b40d1996c69feafab489c6d
noraroyo <noraroyo@opendeusto.es>
2021-01-13 16:19:58 +0100

    curso mas solicitado

--- src/pantallas/CursoPopular.java
+++ src/pantallas/CursoPopular.java
@@ -2,10 +2,13 @@ package pantallas;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
 import javax.swing.border.EmptyBorder;
 
 import java.awt.event.ActionEvent;
@@ -43,6 +46,54 @@ public class CursoPopular extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JLabel lblPopular = new JLabel ("Curso más Solicitado");
+		lblPopular.setFont(new Font("Century Gothic", Font.BOLD, 16));
+		lblPopular.setBounds(114, 33, 350, 50);
+		contentPane.add(lblPopular);
+		
+		
+		
+		JButton btn = new JButton("Volver");
+		btn.setFont(new Font("Century Gothic", Font.BOLD, 16));
+		btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				CursoPopular.this.dispose();
+				papi.setVisible(true);
+			}
+		});
+		btn.setBounds(50, 350, 110, 30);
+		contentPane.add(btn);
+		
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				CursoPopular.this.dispose();
+			}
+		});
+		btnSalir.setBounds(49, 354, 115, 29);
+		contentPane.add(btnSalir);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setFont(new Font("Century Gothic", Font.BOLD, 16));
+		textPane.setBounds(90, 125, 420, 85);
+		contentPane.add(textPane);
+
+		LinkedList<String> curso = cursoPopCalc();
+		String c = null;
+		for (int i = 0; i < curso.size(); i++) {
+			c = curso.get(i) + "\n";
+		}
+		textPane.setText(c);
 	}
 
+	public  LinkedList<String> cursoPopCalc(){
+		LinkedList<String> cursos = new LinkedList<String>();
+		
+		cursos.add("Pilates");
+		cursos.add("Zumba");
+	}
+	
+	
 }
