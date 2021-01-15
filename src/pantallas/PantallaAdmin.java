8f1b7822cf0124386b73c7315336989901221d9b
noraroyo <noraroyo@opendeusto.es>
2021-01-15 17:31:08 +0100

    last modifications pantalla admin

--- src/pantallas/PantallaAdmin.java
+++ src/pantallas/PantallaAdmin.java
@@ -63,7 +63,7 @@ public class PantallaAdmin extends JFrame{
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setFont(new Font("Century Gothic", Font.BOLD, 16));
-		comboBox.setModel(new DefaultComboBoxModel(new String []{"Añadir un Curso", "Modificar un curso", "Curso más solicitado","Email enviar", "Curso: cantidad de solicitudes por usuario"}) );
+		comboBox.setModel(new DefaultComboBoxModel(new String []{"Buscar una factura", "Curso más solicitado", "Curso: cantidad de solicitudes por usuario","Email enviar"}) );
 		comboBox.setBounds(99, 62, 223, 26);
 		contentPane.add(comboBox);
 		
